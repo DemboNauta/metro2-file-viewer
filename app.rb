8cb7f61590efa7e9860bde1b83a1ef5c6651f538
Jaxon Adams <jaxon.adams@loanpro.io>
2024-07-18 15:23:33 -0600

    cleaned up header field view code

--- app.rb
+++ app.rb
@@ -50,119 +50,26 @@ class App
     }
   end
 
+  def create_header_label_element(label_text, data_character_range)
+    horizontal_box {
+      stretchy false
+      label(label_text)
+      label {
+        stretchy false
+        text <= [self.metro2file, :header_str, on_read: ->(header_str) { header_str ? header_str[data_character_range].strip : nil }]
+      }
+    }
+  end
+
   def render_header_info
     # dynamically display header string with data binding
     label("==============\n===Header Info===\n==============") {
       stretchy false
     }
 
-    horizontal_box {
-      stretchy false
-      label('Equifax Identifier:')
-      label {
-        stretchy false
-        text <= [self.metro2file, :header_str, on_read: ->(header_str) { header_str ? header_str[22...32].strip : nil }]
-      }
-    }
-
-    horizontal_box {
-      stretchy false
-      label('Experian Identifier:')
-      label {
-        stretchy false
-        text <= [self.metro2file, :header_str, on_read: ->(header_str) { header_str ? header_str[32...37].strip : nil }]
-      }
-    }
-
-    horizontal_box {
-      stretchy false
-      label('TransUnion Identifier:')
-      label {
-        stretchy false
-        text <= [self.metro2file, :header_str, on_read: ->(header_str) { header_str ? header_str[37...47].strip : nil }]
-      }
-    }
-
-    horizontal_box {
-      stretchy false
-      label('Activity Date:')
-      label {
-        stretchy false
-        text <= [self.metro2file, :header_str, on_read: ->(header_str) { header_str ? parse_date(header_str[47...55].strip) : nil }]
-      }
-    }
-
-    horizontal_box {
-      stretchy false
-      label('Date Created:')
-      label {
-        stretchy false
-        text <= [self.metro2file, :header_str, on_read: ->(header_str) { header_str ? parse_date(header_str[55...63].strip) : nil }]
-      }
-    }
-
-    horizontal_box {
-      stretchy false
-      label('Program Date:')
-      label {
-        stretchy false
-        text <= [self.metro2file, :header_str, on_read: ->(header_str) { header_str ? parse_date(header_str[63...71].strip) : nil }]
-      }
-    }
-
-    horizontal_box {
-      stretchy false
-      label('Program Revision Date:')
-      label {
-        stretchy false
-        text <= [self.metro2file, :header_str, on_read: ->(header_str) { header_str ? parse_date(header_str[71...79].strip) : nil }]
-      }
-    }
-
-    horizontal_box {
-      stretchy false
-      label('Reporter Name:')
-      label {
-        stretchy false
-        text <= [self.metro2file, :header_str, on_read: ->(header_str) { header_str ? header_str[79...119].strip : nil }]
-      }
-    }
-
-    horizontal_box {
-      stretchy false
-      label('Reporter Address:')
-      label {
-        stretchy false
-        text <= [self.metro2file, :header_str, on_read: ->(header_str) { header_str ? header_str[119...215].strip : nil }]
-      }
-    }
-
-    horizontal_box {
-      stretchy false
-      label('Reporter Phone:')
-      label {
-        stretchy false
-        text <= [self.metro2file, :header_str, on_read: ->(header_str) { header_str ? header_str[215...225].strip : nil }]
-      }
-    }
-
-    horizontal_box {
-      stretchy false
-      label('Program Creator:')
-      label {
-        stretchy false
-        text <= [self.metro2file, :header_str, on_read: ->(header_str) { header_str ? header_str[225...265].strip : nil }]
-      }
-    }
-
-    horizontal_box {
-      stretchy false
-      label('Program Version:')
-      label {
-        stretchy false
-        text <= [self.metro2file, :header_str, on_read: ->(header_str) { header_str ? header_str[265...426].strip : nil }]
-      }
-    }
+    Metro2File::HEADER_FIELDS.each do |header_field|
+      create_header_label_element(header_field[0], header_field[1])
+    end
   end
 
 end
--- lib/metro2file.rb
+++ lib/metro2file.rb
@@ -4,7 +4,22 @@ class Metro2File
 
   include ParseUtils
 
-  attr_accessor :header_str, :trailer_str, :accounts_strs
+  attr_accessor :header_str, :header_fields, :trailer_str, :accounts_strs
+
+  HEADER_FIELDS = [
+    ['Equifax Identifier', 22...32],
+    ['TransUnion Identifier', 32...42],
+    ['Experian Identifier', 42...47],
+    ['Activity Date', 47...55],
+    ['Date Created', 55...63],
+    ['Program Date', 63...71],
+    ['Program Revision Date', 71...79],
+    ['Reporter Name', 79...119],
+    ['Reporter Address', 119...215],
+    ['Reporter Phone', 215...225],
+    ['Software Vendor Name', 225...265],
+    ['Software Version Number', 265...426],
+  ]
 
   def from_file(file_path)
     File.open(file_path, 'r') do |file|
