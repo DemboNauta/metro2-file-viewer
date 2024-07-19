c6eba1fbb23383fad77c64853b40188d50bd224e
Jaxon Adams <jaxon.adams@loanpro.io>
2024-07-19 11:41:38 -0600

    added record parsing logic to Metro2File class

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,7 @@ class Metro2File {
 
         this.parseHeader();
         this.parseTrailer();
+        this.parseRecords();
     }
 
     parseHeader() {
@@ -91,6 +92,137 @@ class Metro2File {
         }
 
         this.trailer = trailerObj;
+    }
+
+    parseRecords() {
+        const records = this.recordStrs;
+
+        const recordObjs = records.map(record => {
+            const recordObj = {
+                recordDescriptor: record.slice(0, 4),
+                processingIndicator: record.slice(4, 5),
+                timestampDate: parseMetro2Date(record.slice(5, 13)),
+                correctionIndicator: record.slice(19, 20),
+                identificationNumber: record.slice(20, 40),
+                cycleNumber: record.slice(40, 42),
+                consumerAccountNumber: record.slice(42, 72),
+                portfolioType: record.slice(72, 73),
+                accountType: record.slice(73, 75),
+                dateOpened: parseMetro2Date(record.slice(75, 83)),
+                creditLimit: parseInt(record.slice(84, 93)),
+                highestCredit: parseInt(record.slice(93, 101)),
+                termsDuration: record.slice(101, 104),
+                termsFrequency: record.slice(104, 105),
+                scheduledMonthlyPayment: parseInt(record.slice(105, 114)),
+                actualPaymentAmount: parseInt(record.slice(114, 123)),
+                accountStatus: record.slice(123, 125),
+                paymentRating: record.slice(125, 126),
+                paymentHistoryProfile: record.slice(126, 150),
+                specialComment: record.slice(150, 152),
+                complianceConditionCode: record.slice(152, 154),
+                currentBalance: parseInt(record.slice(154, 163)),
+                amountPastDue: parseInt(record.slice(163, 172)),
+                originalChargeOffAmount: parseInt(record.slice(172, 181)),
+                billingDate: parseMetro2Date(record.slice(181, 189)),
+                firstDelinquencyDate: parseMetro2Date(record.slice(189, 197)),
+                closedDate: parseMetro2Date(record.slice(197, 205)),
+                lastPaymentDate: parseMetro2Date(record.slice(205, 213)),
+                consumerTransactionType: record.slice(230, 231),
+                surname: record.slice(231, 256),
+                firstName: record.slice(256, 276),
+                middleName: record.slice(276, 296),
+                generationCode: record.slice(296, 297),
+                socialSecurityNumber: record.slice(297, 306),
+                dateOfBirth: parseMetro2Date(record.slice(306, 314)),
+                telephoneNumber: record.slice(314, 324),
+                ecoaCode: record.slice(324, 325),
+                consumerInformationIndicator: record.slice(325, 327),
+                countryCode: record.slice(327, 329),
+                firstLineAddress: record.slice(329, 361),
+                secondLineAddress: record.slice(361, 393),
+                city: record.slice(393, 413),
+                state: record.slice(413, 415),
+                postalCode: record.slice(415, 424),
+                addressIndicator: record.slice(424, 425),
+                residenceCode: record.slice(425, 426),
+                j1SegmentIdentifier: null,
+                j1ConsumerTransactionType: null,
+                j1Surname: null,
+                j1FirstName: null,
+                j1MiddleName: null,
+                j1GenerationCode: null,
+                j1SocialSecurityNumber: null,
+                j1DateOfBirth: null,
+                j1TelephoneNumber: null,
+                j1EcoaCode: null,
+                j1ConsumerInformationIndicator: null,
+                j2SegmentIdentifier: null,
+                j2ConsumerTransactionType: null,
+                j2Surname: null,
+                j2FirstName: null,
+                j2MiddleName: null,
+                j2GenerationCode: null,
+                j2SocialSecurityNumber: null,
+                j2DateOfBirth: null,
+                j2TelephoneNumber: null,
+                j2EcoaCode: null,
+                j2ConsumerInformationIndicator: null,
+                j2CountryCode: null,
+                j2FirstLineAddress: null,
+                j2SecondLineAddress: null,
+                j2City: null,
+                j2State: null,
+                j2PostalCode: null,
+                j2AddressIndicator: null,
+                j2ResidenceCode: null,
+            };
+
+            // add j1 and j2 fields if the record is a consumer segment
+            if (recordObj.recordDescriptor === '0526') {
+                recordObj.j1SegmentIdentifier = record.slice(426, 428);
+                recordObj.j1ConsumerTransactionType = record.slice(428, 429);
+                recordObj.j1Surname = record.slice(429, 454);
+                recordObj.j1FirstName = record.slice(454, 474);
+                recordObj.j1MiddleName = record.slice(474, 494);
+                recordObj.j1GenerationCode = record.slice(494, 495);
+                recordObj.j1SocialSecurityNumber = record.slice(495, 504);
+                recordObj.j1DateOfBirth = parseMetro2Date(record.slice(504, 512));
+                recordObj.j1TelephoneNumber = record.slice(512, 522);
+                recordObj.j1EcoaCode = record.slice(522, 523);
+                recordObj.j1ConsumerInformationIndicator = record.slice(523, 525);
+            } else if (recordObj.recordDescriptor === '0626') {
+                recordObj.j2SegmentIdentifier = record.slice(426, 428);
+                recordObj.j2ConsumerTransactionType = record.slice(428, 429);
+                recordObj.j2Surname = record.slice(429, 454);
+                recordObj.j2FirstName = record.slice(454, 474);
+                recordObj.j2MiddleName = record.slice(474, 494);
+                recordObj.j2GenerationCode = record.slice(494, 495);
+                recordObj.j2SocialSecurityNumber = record.slice(495, 504);
+                recordObj.j2DateOfBirth = parseMetro2Date(record.slice(504, 512));
+                recordObj.j2TelephoneNumber = record.slice(512, 522);
+                recordObj.j2EcoaCode = record.slice(522, 523);
+                recordObj.j2ConsumerInformationIndicator = record.slice(523, 525);
+                recordObj.j2CountryCode = record.slice(525, 527);
+                recordObj.j2FirstLineAddress = record.slice(527, 559);
+                recordObj.j2SecondLineAddress = record.slice(559, 591);
+                recordObj.j2City = record.slice(591, 611);
+                recordObj.j2State = record.slice(611, 613);
+                recordObj.j2PostalCode = record.slice(613, 622);
+                recordObj.j2AddressIndicator = record.slice(622, 623);
+                recordObj.j2ResidenceCode = record.slice(623, 624);
+            }
+
+            // trim extra whitespace from each object value if it is a string
+            for (let key in recordObj) {
+                if (typeof recordObj[key] === 'string') {
+                    recordObj[key] = recordObj[key].trim();
+                }
+            }
+
+            return recordObj;
+        });
+
+        this.records = recordObjs;
     }
 }
 
@@ -181,6 +313,10 @@ const displayTrailerInfo = trailer => {
     createBootstrapTable(trailerInfo, trailer);
 }
 
+const displayRecordsInfo = records => {
+    console.log(records);
+}
+
 const parseMetro2File = fileContent => {
     const lines = fileContent.split('\n');
 
@@ -191,6 +327,7 @@ const parseMetro2File = fileContent => {
     const metro2File = new Metro2File(header, records, trailer);
 
     displayHeaderInfo(metro2File.header);
+    displayRecordsInfo(metro2File.records);
     displayTrailerInfo(metro2File.trailer);
 }
 
