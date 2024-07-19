b562b5728a661774ea98c059bb5c27d9f8ff0ca4
Jaxon Adams <jaxon.adams@loanpro.io>
2024-07-19 09:16:51 -0600

    initial electron files

--- Gemfile
+++
@@ -1,2 +0,0 @@
-source "https://rubygems.org"
-gem "glimmer-dsl-libui", "~> 0.12.2"
--- lib/parseutils.rb
+++
@@ -1,8 +0,0 @@
-module ParseUtils
-
-  def parse_date(date_str)
-    # parse a date from the format MMDDYYYY to MM/DD/YYYY
-    date_str[0..1] + '/' + date_str[2..3] + '/' + date_str[4..7]
-  end
-
-end
